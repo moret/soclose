0c6a5d75185606f687153950a05926366c49c8c3
Danilo Moret <github@moret.pro.br>
2017-08-11 07:56:45 -0300

    Expand distance input validation and insurances on latin characters

--- spec/distanceSpec.js
+++ spec/distanceSpec.js
@@ -26,3 +26,48 @@ describe('corner cases', () => {
     expect(distance('abacate', 'abacate')).toBe(0);
   });
 });
+
+describe('invalid inputs', () => {
+  it('rejects undefined, null or non-string parameters', () => {
+    [
+      null,
+      undefined,
+      true,
+      false,
+      12345,
+      { banana: 'abacate' },
+      [ 'b', 'a', 'n', 'a', 'n', 'a' ],
+    ].forEach(invalidInput => {
+      expect(() => distance('banana', invalidInput)).toThrowError(TypeError, 'Only strings accepted');
+      expect(() => distance(invalidInput, 'abacate')).toThrowError(TypeError, 'Only strings accepted');
+    });
+  });
+
+  it('rejects words too large', () => {
+    // https://en.wikipedia.org/wiki/Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu
+    const longWord = 'Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu';
+    expect(() => distance(longWord, 'abacate')).toThrowError(TypeError, 'Only strings with 50 characters or less accepted');
+    expect(() => distance('banana', longWord)).toThrowError(TypeError, 'Only strings with 50 characters or less accepted');
+  });
+})
+
+describe('latin characters', () => {
+  it('does not count accents, tildes and cedillas as different from their ascii collations', () => {
+    expect(distance('açaí', 'acai')).toBe(0);
+    expect(distance('mamão', 'mamao')).toBe(0);
+  });
+
+  it('handles all accents, tildes and cedillas as their ascii collations', () => {
+    expect(distance('áàâãä', 'aaaaa')).toBe(0);
+    expect(distance('éèêẽë', 'eeeee')).toBe(0);
+    expect(distance('íìîĩï', 'iiiii')).toBe(0);
+    expect(distance('óòôõö', 'ooooo')).toBe(0);
+    expect(distance('úùûũü', 'uuuuu')).toBe(0);
+    expect(distance('ñỹç', 'nyc')).toBe(0);
+  });
+
+  it('counts accent distances as one each just like any other letter', () => {
+    expect(distance('caçador', 'catador')).toBe(1);
+    expect(distance('pará', 'pare')).toBe(1);
+  });
+});
--- src/distance.js
+++ src/distance.js
@@ -1,5 +1,11 @@
 import levenshtein from 'fast-levenshtein';
 
 export default function distance(wordA, wordB) {
+  if (typeof wordA != 'string' || typeof wordB != 'string') {
+    throw new TypeError('Only strings accepted');
+  }
+  if (wordA.length > 50 || wordB.length > 50) {
+    throw new TypeError('Only strings with 50 characters or less accepted');
+  }
   return levenshtein.get(wordA, wordB, { useCollator: true });
 }
